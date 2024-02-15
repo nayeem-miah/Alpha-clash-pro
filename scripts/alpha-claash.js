5f573e3f9c137894456ce74949b7544a7e64b95e
nayeem-miah <nayeem5113a@gmail.com>
2024-02-15 18:21:32 +0600

    added-clash

--- scripts/alpha-claash.js
+++ scripts/alpha-claash.js
@@ -18,7 +18,115 @@
 
 // }
 
-function play(){
-    HideElementById('home-screen');
-    showElementById('play-ground');
+function handlerKeyboardKeyUpEvent(event){
+    const playerPress=event.key;
+
+    // key player is expected to press
+    const  currentAlphabetElement=document.getElementById('current-alphabet');
+    const currentAlphabet=currentAlphabetElement.innerText;
+    const expectedAlphabet=currentAlphabet.toLowerCase();
+
+    // check right or wrong key pressed
+    if(playerPress===expectedAlphabet){
+        console.log('you got a point !! ');
+// step  1:
+        const currentScore=getTextElementValueById('current-score');
+        console.log(currentScore);
+//  step 2:
+      const newScore= currentScore+1;
+
+// step 3: set value:
+      setTextElementById('current-score',newScore);
+
+// ----------------------------------------------------->
+        // update score :
+        // // 1.get the current score
+        // const currentScoreElement=document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore=parseInt(currentScoreText);
+        // console.log(currentScore);
+        
+        
+        
+        // // 2.increase the score by 1;
+        // const newScore = currentScore+1;
+
+        // // console.log(newScore);
+        // // 3.show the update score
+        
+        // currentScoreElement.innerText=newScore;
+        // start a new round
+
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('durr vaiya or apu....... please right key now');
+
+
+        const currentLife=getTextElementValueById('current-life');
+       
+
+        // step 2:
+        const newLife =currentLife-1;
+        // step 2
+
+        setTextElementById('current-life',newLife);
+        if(newLife === 0){
+            gameOver();
+        }
+// --------------------------------------------------------->
+        // // step.1: get the current number
+        // const currentLifeElement=document.getElementById('current-life');
+        // const currentLifeText=currentLifeElement.innerText;
+        // const currentLife=parseInt(currentLifeText);
+        // console.log(currentLife);
+        // // step 2:reduce the life count
+        // const newLife=currentLife-1;
+        // // step 3: display the updated life count
+        // currentLifeElement.innerText=newLife;
+    }
+    
+    
+    
+  
 }
+// capture keyboard handler     
+document.addEventListener('keyup',handlerKeyboardKeyUpEvent);
+
+function continueGame(){
+    // generate a random alphabet 
+    const alphabet=getARandomAlphabet();
+    // console.log('your random alphabet ',alphabet);
+
+    // set randomly generate alphabet to the screen(show it)
+    const currentAlphabetElement=document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText=alphabet;
+
+    // set background color
+    addBackgroundColorById(alphabet);
+}
+
+function play(){
+ // hide everything show only playground
+
+    HideElementById('home-screen');
+    HideElementById('final-score');
+    showElementById('play-ground');
+    // reset score and life
+    setTextElementById('current-life', 5);
+    setTextElementById('current-score',0);
+    continueGame();
+}
+
+function gameOver(){
+   
+    HideElementById('play-ground');
+    
+    showElementById('final-score')
+}
+// function playAgain(){
+//     HideElementById('final-score');
+//     showElementById('play-ground');
+//     continueGame();
+// }
